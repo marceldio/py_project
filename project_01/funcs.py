7096ba0ef72aee490fdcbf64115e5354fc64d57d
Marcel Dio <marceldiomare@gmail.com>
2024-03-23 05:42:29 +0300

    create main.py

--- project_01/funcs.py
+++ project_01/funcs.py
@@ -1,8 +1,13 @@
 def sum_(a, b):
     sum_ = a + b
-    print(sum_)
+    print(f'Сумма: {sum_}')
+
+def dif_(a, b):
+    dif_ = a - b
+    print(f'Разность: {dif_}')
 
 
 a, b = map(int, input('Введите два числа через пробел: ').split())
 
 sum_(a, b)
+dif_(a, b)
